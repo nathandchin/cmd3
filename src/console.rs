311c5b867c90a5862c7351fab7fcf5d2f445b958
Nathan Chin <nathanchin@nathanchin.com>
2025-05-01 22:51:08 -0400

    Refactor completion to its own module, get rid of unnecessary Box

--- src/console.rs
+++ src/console.rs
@@ -7,12 +7,10 @@ use std::{
     rc::Rc,
 };
 
-use rustyline::{
-    completion::{Completer, Pair},
-    error::ReadlineError,
-    Completer, Helper, Highlighter, Hinter, Validator,
-};
+use rustyline::{error::ReadlineError, Completer, Helper, Highlighter, Hinter, Validator};
 use thiserror::Error;
+
+use crate::completion::CommandCompleter;
 
 #[derive(Error, Debug)]
 pub enum ConsoleError {
@@ -34,140 +32,12 @@ pub enum ConsoleError {
     BrokenPipeError(Box<ConsoleError>),
 }
 
+pub(crate) type CommandSet = Rc<RefCell<HashMap<String, Box<dyn Command>>>>;
+
 #[derive(Helper, Completer, Validator, Hinter, Highlighter)]
 struct ConsoleHelper {
     #[rustyline(Completer)]
     completer: CommandCompleter,
-}
-
-type CommandSet = Rc<RefCell<HashMap<String, Box<dyn Command>>>>;
-
-struct CommandCompleter {
-    commands: CommandSet,
-}
-
-impl CommandCompleter {
-    fn new(commands: CommandSet) -> Self {
-        Self { commands }
-    }
-}
-
-impl Completer for CommandCompleter {
-    type Candidate = Pair;
-
-    fn complete(
-        &self,
-        line: &str,
-        pos: usize,
-        _ctx: &rustyline::Context<'_>,
-    ) -> rustyline::Result<(usize, Vec<Pair>)> {
-        let orig_pos = pos;
-        let (line, pos) = if let Some(i) = line.rfind('|') {
-            (&line[i + 1..], pos - i - 1)
-        } else {
-            (line, pos)
-        };
-
-        let subtokens = match shlex::split(&line[0..pos]) {
-            Some(o) => o,
-            None => return Ok((pos, vec![])),
-        };
-
-        let is_first_word =
-            subtokens.len() < 2 && !line[0..pos].contains(|o: char| o.is_whitespace());
-
-        let command_set = &self.commands.borrow();
-
-        if is_first_word {
-            // We are completing the name of a command
-            let mut res = vec![];
-            for command in self.commands.borrow().keys() {
-                if command.starts_with(line) {
-                    res.push(Pair {
-                        display: command.to_string(),
-                        replacement: command.to_string(),
-                    });
-                }
-            }
-
-            Ok((orig_pos.saturating_sub(line.len()), res))
-        } else {
-            // We are completing an argument to a command
-            let command = match command_set.get(&subtokens[0]) {
-                Some(c) => c,
-                None => return Ok((orig_pos, vec![])), // Unrecognized command
-            };
-
-            let mut completions: Vec<Pair> = vec![];
-            let parser = command.get_parser();
-
-            if line.chars().nth(pos - 1).unwrap().is_whitespace() {
-                // Cursor is not on a word, show all positional args
-                for arg in parser.get_positionals() {
-                    completions.push(Pair {
-                        display: arg.get_id().to_string(),
-                        replacement: "".to_string(), // Don't actually complete these metavars
-                    });
-                }
-                Ok((orig_pos, completions))
-            } else {
-                let word = subtokens.last().unwrap();
-
-                if word.starts_with("--") {
-                    // Long form
-                    for arg in parser.get_opts() {
-                        if let Some(long) = arg.get_long() {
-                            // Only one possibility: long form
-                            let replacement = format!("--{}", long);
-
-                            if replacement.starts_with(word) {
-                                completions.push(Pair {
-                                    display: format!("[{}]", replacement),
-                                    replacement,
-                                });
-                            }
-                        }
-                    }
-                    Ok((orig_pos - word.len(), completions))
-                } else if word.starts_with("-") {
-                    // Short OR long form
-                    for arg in parser.get_opts() {
-                        let long = arg.get_long();
-                        let short = arg.get_short();
-
-                        // Can be any of long+short, long only, or short only
-                        let (display, replacement) =
-                            if let (Some(long), Some(short)) = (long, short) {
-                                (format!("[-{}, --{}]", short, long), format!("-{} ", short))
-                            } else if let Some(long) = long {
-                                (format!("[--{}]", long), format!("--{} ", long))
-                            } else if let Some(short) = short {
-                                (format!("[-{}]", short), format!("-{} ", short))
-                            } else {
-                                // Trying to use such an arg will be a runtime
-                                // error when the parser is invoked, but it
-                                // won't appear in the result of `get_opts()` so
-                                // it doesn't come into play here.
-                                unreachable!("Arg must have at least one of long or short form");
-                            };
-
-                        if replacement.starts_with(word) {
-                            completions.push(Pair {
-                                display,
-                                replacement,
-                            });
-                        }
-                    }
-                    Ok((orig_pos - word.len(), completions))
-                } else {
-                    // Must be a positional arg, don't bother completing them
-                    // since their names are just metavars. Possibly implement
-                    // custom completers here?
-                    Ok((orig_pos, vec![]))
-                }
-            }
-        }
-    }
 }
 
 pub trait Command {
@@ -194,7 +64,7 @@ enum Runnable<'a> {
         args: Vec<String>,
     },
     Command {
-        cmd: &'a Box<dyn Command>,
+        cmd: &'a dyn Command,
         args: clap::ArgMatches,
     },
 }
@@ -315,7 +185,10 @@ impl Console {
                         }
                     };
 
-                    runnables.push_back(Runnable::Command { cmd, args: matches });
+                    runnables.push_back(Runnable::Command {
+                        cmd: cmd.as_ref(),
+                        args: matches,
+                    });
                 } else {
                     eprintln!("{}", ConsoleError::UnrecognizedCommand(tokens[0].clone()));
                     continue 'command_loop;
